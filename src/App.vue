51e9a1bf39d562068753b71fb12d939231abfd5d
Jonathan Adebola <conektbishop@gmail.com>
2018-07-03 19:13:40 +0100

    My first commit on this project

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="createdQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deletedQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a quote to deleted it!</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+import Header from './components/Header.vue';
+export default {
+  data: function(){
+    return {
+      quotes: [],
+      maxQuotes: 10
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  },
+  methods: {
+    createdQuote(quote){
+      if(this.quotes.length > this.maxQuotes){
+        return alert("Please delete some quotes before adding new!");
+      }
+      this.quotes.push(quote);
+    },
+    deletedQuote(index){
+      this.quotes.splice(index, 1);
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="row">
+        <form action="">
+            <div class="col-sm-8 col-sm-offset-2 xs-12 col-md-6 col-md-offset-3 form-group">
+                <label for="">Quote</label>
+                <textarea class="form-control" name="" id="" cols="10" rows="5" v-model="quote"></textarea>
+            </div>
+            <div class="col-sm-8 col-sm-offset-2 xs-12 col-md-6 col-md-offset-3 form-group">
+                <button class="btn btn-primary form-control" @click.prevent="createNew">Add Quote</button>
+            </div>
+        </form>
+        
+    </div>
+</template>
+<script>
+export default {
+    data: function(){
+        return {
+            quote: ''
+        }
+    },
+    methods:{
+        createNew(){
+            if(this.quote=='' || this.quote.length <= 0){
+                return false;
+            }else{
+                this.$emit('quoteAdded', this.quote);
+                this.quote= '';
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+form{
+    padding: 10px;
+}
+
+</style>
+
+
